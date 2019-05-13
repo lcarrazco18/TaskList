4dd725c50bba3587b5ecc79290b004b8e1732978
Luis Carrazco <luiscarrazco@gmail.com>
2019-05-12 21:50:25 -0600

    UI and add task item

--- app.js
+++ app.js
@@ -6,3 +6,41 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
+// Load all Event listeners
+loadEventListeners();
+
+// Load all Event listeners
+function loadEventListeners() {
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// Add task
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fas fa-times"></i>'
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+
+    // Clear Input
+    taskInput.value = '';
+
+    e.preventDefault();
+}
